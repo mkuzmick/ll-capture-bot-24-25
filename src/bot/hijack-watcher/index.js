5b8b28760fbe063bdc2b7fbbaea6c1522c640bd3
process black <process_black@processs-Mac-Pro.local>
2024-11-08 14:25:05 -0500

    Updates:

--- src/bot/hijack-watcher/index.js
+++ src/bot/hijack-watcher/index.js
@@ -14,9 +14,11 @@ const axios = require('axios');
 // const rainbowChannel = "C07K2TEFQFP"
 
 // temp gened1145 as rainbow
-const rainbowChannel = "C07T0GUUKC0"
+const rainbowChannel = "C07K2TEFQFP"
 
+// C07K2TEFQFP
 // logs channel as rainbow 
+
 // const rainbowChannel = "C07K2TEFQFP"
 
 
@@ -89,7 +91,7 @@ async function uploadImageToRainbow(client, scene, filename, imagePath, fileSize
 }
 
 
-const tdmConfig = {
+const designLabConfig = {
   scenes: [
     {
       name: "The Control Room",
@@ -98,13 +100,21 @@ const tdmConfig = {
       color: "gray",
       icon: "https://files.slack.com/files-pri/T0HTW3H0V-F06T3AKR886/red.webp?pub_secret=9c8372302a"
     },
-    // {
-    //   name: "main table",
-    //   channel: "C07K2TEFQFP",
-    //   track: "a8k_01",
-    //   color: "gray",
-    //   icon: "https://files.slack.com/files-pri/T0HTW3H0V-F07SKQ0TGU9/main_table.webp?pub_secret=48fa3168df"
-    // },
+    {
+      name: "main table 1",
+      channel: "C07R9CSJNG0",
+      track: "a8k_09",
+      color: "gray",
+      icon: "https://files.slack.com/files-pri/T0HTW3H0V-F07SKQ0TGU9/main_table.webp?pub_secret=48fa3168df"
+    },
+    {
+      name: "main table 2",
+      channel: "C07R9CSJNG0",
+      track: "a8k_10",
+      color: "gray",
+      icon: "https://files.slack.com/files-pri/T0HTW3H0V-F07SKQ0TGU9/main_table.webp?pub_secret=48fa3168df"
+    },
+    
     {
       name: "Small Studio",
       channel: "C07R9CSJNG0",
@@ -119,15 +129,106 @@ const tdmConfig = {
     //   color: "gray"
     // },
     {
-      name: "Main Stage",
-      channel: "C07T0JG9J7J",
-      track: "a8k_09",
+      name: "Red",
+      channel: "C07VB8S1PNF",
+      track: "a8k_08",
       color: "red",
       icon: "https://files.slack.com/files-pri/T0HTW3H0V-F06T3AKR886/red.webp?pub_secret=9c8372302a"
     },
     {
-      name: "Main Table",
-      channel: "C07T0J4RZ24",
+      name: "Purple",
+      channel: "C07VBBB6L1Y",
+      track: "a8k_07",
+      color: "purple",
+      icon: "https://files.slack.com/files-pri/T0HTW3H0V-F06TCDDKGTE/purple.webp?pub_secret=95f7661c66"
+    },
+    // {
+    //   name: "comp-lit-200-orange",
+    //   channel: "C07RE2V1SDN",
+    //   track: "a8k_09",
+    //   color: "orange",
+    //   icon: "https://files.slack.com/files-pri/T0HTW3H0V-F06TYNY4G5N/orange.webp?pub_secret=b8a0aec13a"
+    // },
+    // {
+    //   name: "comp-lit-200-yellow",
+    //   channel: "C07QNCAU8LV",
+    //   track: "a8k_05",
+    //   color: "yellow",
+    //   icon: "https://files.slack.com/files-pri/T0HTW3H0V-F06SVCN5HK9/yellow.webp?pub_secret=8100e38384"
+    // },
+    // {
+    //   name: "comp-lit-200-green",
+    //   channel: "C07QTQ7MKFW",
+    //   track: "a8k_10",
+    //   color: "green",
+    //   icon: "https://files.slack.com/files-pri/T0HTW3H0V-F06SVCR9895/green.webp?pub_secret=ac944d3bd2"
+    // },
+    // {
+    //   name: "main table",
+    //   channel: "C07LRH22JGN",
+    //   track: "a8k_01",
+    //   color: "gray",
+    //   icon: "https://files.slack.com/files-pri/T0HTW3H0V-F07QPAAE485/dall__e_2024-10-07_14.06.08_-_a_retro-futuristic_butcher_block_main_table_in_a_field_of_vibrant__multicolored_flowers__with_a_metal_base_and_colorful_markers__blank_paper_on_top-main-table.webp?pub_secret=6d037790f3"
+    // },
+    // {
+    //   name: "comp-lit-200-blue",
+    //   channel: "C07R40FDHB3",
+    //   track: "a8k_07",
+    //   color: "blue",
+    //   icon: "https://files.slack.com/files-pri/T0HTW3H0V-F06T3AHJ5ML/blue.webp?pub_secret=a5b1b2376e"
+    // },
+    // {
+    //   name: "comp-lit-200-rainbow",
+    //   channel: "C07RE3C3FQQ",
+    //   track: "a8k_11",
+    //   color: "rainbow"
+    //   icon: "https://files.slack.com/files-pri/T0HTW3H0V-F07QUJRQQLU/dall__e_2024-10-07_13.53.48_-_a_retro-futuristic_computer_in_a_field_of_colorful_wildflowers_inspired_by_rainbow_hues__with_sleek_rounded_edges_and_a_subtle_rainbow_reflection_on_i-rainbow.webp?pub_secret=14aa4b557e"
+    // },
+  ]
+};
+
+
+
+
+const tdmConfig = {
+  scenes: [
+    {
+      name: "The Control Room",
+      channel: "C07QLJSM81G",
+      track: "a8k_03",
+      color: "gray",
+      icon: "https://files.slack.com/files-pri/T0HTW3H0V-F06T3AKR886/red.webp?pub_secret=9c8372302a"
+    },
+    {
+      name: "main table",
+      channel: "C07K2TEFQFP",
+      track: "a8k_01",
+      color: "gray",
+      icon: "https://files.slack.com/files-pri/T0HTW3H0V-F07SKQ0TGU9/main_table.webp?pub_secret=48fa3168df"
+    },
+    {
+      name: "Small Studio",
+      channel: "C07R9CSJNG0",
+      track: "a8k_05",
+      color: "green",
+      // icon: "https://files.slack.com/files-pri/T0HTW3H0V-F07QP9S0NKF/dall__e_2024-10-07_14.03.03_-_a_retro-futuristic_small_studio_setup_in_a_field_of_vibrant_flowers__with_two_microphones_subtly_placed_in_front_of_a_green_or_black_backdrop._the_des-small-studio.webp?pub_secret=913c691b70"
+    },
+    // {
+    //   name: "default",
+    //   channel: "C07K2TEFQFP",
+    //   track: "a8k_11",
+    //   color: "gray"
+    // },
+    {
+      name: "Mic 2",
+      channel: "C07VB8S1PNF",
+      track: "a8k_08",
+      color: "red",
+      icon: "https://files.slack.com/files-pri/T0HTW3H0V-F06T3AKR886/red.webp?pub_secret=9c8372302a"
+    },
+    {
+      name: "Mic 1",
+      channel: "C07VBBB6L1Y",
       track: "a8k_07",
       color: "purple",
       icon: "https://files.slack.com/files-pri/T0HTW3H0V-F06TCDDKGTE/purple.webp?pub_secret=95f7661c66"
@@ -267,7 +368,7 @@ const gened1145Config = {
   ]
 };
 
-const customConfig = gened1145Config;
+const customConfig = designLabConfig;
 
 const handleFile = (filePath) => {
   const fileName = path.basename(filePath, path.extname(filePath)); // Extract the file name without extension
